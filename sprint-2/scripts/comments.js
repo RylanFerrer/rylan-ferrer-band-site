380bef13ca3a07cf3784330f4351337c71c0f5dc
RylanFerrer <rylanferrer@gmail.com>
2019-10-20 18:14:12 -0700

    added random avatars to comments

--- sprint-2/scripts/comments.js
+++ sprint-2/scripts/comments.js
@@ -1,46 +1,64 @@
+const avatars = ["./assets/images/avatars/avatar-1.png","./assets/images/avatars/avatar-2.png","./assets/images/avatars/avatar-3.png",
+"./assets/images/avatars/avatar-3.png", "./assets/images/avatars/avatar-4.png","./assets/images/avatars/avatar-5.png","./assets/images/avatars/avatar-6.png",
+"./assets/images/avatars/avatar-7.png","./assets/images/avatars/avatar-8.png","./assets/images/avatars/avatar-9.png"];
 const comments = [
     
   {
-      avatar: "./assets/images/avatar.jpg",
+      avatar: "",
       name: "Micheal Lyons",
       date: "12/18/2018",
       comment:
         "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed."
     },
     {
-      avatar: "./assets/images/avatar.jpg",
+      avatar: "",
       name: "Gary Wong",
       date: "12/12/2018",
       comment:
         "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!"
     },
     {
-      avatar: "./assets/images/avatar.jpg",
+      avatar: "./assets/images/avatars/guy.jpg",
       name: "Theodore Duncan",
       date: "11/15/2018",
       comment:
         "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!"
     }
    ];
-
+  
 
    const commentSection = document.getElementById("comments-display");
    const form = document.getElementById("form-section");
    
    //comment section initialization
    fillCommentSection(comments);   
-   
+
+   function makeNewAvatar(avatars)
+   {
+     let random = Math.floor(Math.random() * avatars.length);
+      return avatars[random];
+   }
    form.addEventListener("submit", submitEvent => {
 
     submitEvent.preventDefault();
      //Get the date for today
     let d = new Date();
-    let today = d.getDate() + "/" + d.getMonth() + "/" + d.getFullYear(); 
+    let month = d.getMonth() + 1;
+    let today = d.getDate() + "/" + month  + "/" + d.getFullYear(); 
 
     //create a new comment to push into the front of the comments array
     let comment = {};
-
+    
+   
     comment.name = submitEvent.target.firstName.value;
+    if(comment.name === "Rylan Ferrer" || comment.name === "Rylan")
+    {
+      comment.avatar = "./assets/images/avatars/rylan.jpg";
+    }
+    else 
+    {
+      comment.avatar = makeNewAvatar(avatars);
+    }
     comment.date = today;
     comment.comment = submitEvent.target.comment.value;
     console.log(comment);
@@ -49,6 +67,8 @@ const comments = [
     //Clear the comment section of all comments and then fill them back up with the comment
     clearComments(commentSection);
     fillCommentSection(comments);
+
+    submitEvent.target.reset();
   });
 
    function clearComments(section)
@@ -76,7 +96,14 @@ const comments = [
    function fillCommentSection(data) 
    {
         for(var i =0; i < data.length; i++)
-        {
+        { 
+           
+            if(data[i].avatar === "")
+            {
+              
+              data[i].avatar = makeNewAvatar(avatars);
+          
+            }
             addCommentInfo(commentSection,comments[i]);
         }
     }
